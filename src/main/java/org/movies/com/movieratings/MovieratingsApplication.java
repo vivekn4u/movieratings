dae29a01e456ee5f6ec37579b64965d750df497f
Vivek Nakalgaonkar <vivekn4u@gmail.com>
2021-10-29 14:08:23 +0530

    initial version

---
+++ src/main/java/org/movies/com/movieratings/MovieratingsApplication.java
@@ -0,0 +1,13 @@
+package org.movies.com.movieratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieratingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieratingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/movies/com/movieratings/MovieratingsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.movies.com.movieratings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieratingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
