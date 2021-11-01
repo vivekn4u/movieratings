3241fa98991d2f6fa41a6a2cbf3551bf58adffa5
Vivek Nakalgaonkar <vivekn4u@gmail.com>
2021-11-01 14:58:54 +0530

    comment added

--- src/main/java/org/movies/com/movieratings/MovieratingsApplication.java
+++ src/main/java/org/movies/com/movieratings/MovieratingsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieratingsApplication {
 
+	/** Main method */
 	public static void main(String[] args) {
 		SpringApplication.run(MovieratingsApplication.class, args);
 	}
